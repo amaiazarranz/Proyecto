e7eb532d0e2a38a1764c13ca253017fb1632bea5
amaiazarranz <amaia.zarranz@opendeusto.es>
2019-10-25 21:15:11 +0200

    he creado la clase falta leve

---
+++ src/frontend/FaltaLeve.java
@@ -0,0 +1,152 @@
+package frontend;
+
+import java.awt.BorderLayout;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+
+import sqlite.DBManager;
+import sqlite.SelectData;
+import usuarios.Estudiante;
+import usuarios.Persona;
+import usuarios.Profesor;
+import usuarios.Trabajador;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+
+
+public class FaltaLeve extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JList list;
+
+
+	/**
+	 * Create the frame.
+	 */
+	
+	public FaltaLeve(ArrayList <Estudiante> diccionarioEstudiantes, Persona profe, VentanaProfesor atras)
+	{
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblPonerUnaFalta = new JLabel("Poner una falta leve:");
+		lblPonerUnaFalta.setBounds(138, 16, 176, 20);
+		contentPane.add(lblPonerUnaFalta);
+		
+		JLabel lblNmeroDeFaltas = new JLabel("N\u00FAmero de faltas leves:");
+		lblNmeroDeFaltas.setBounds(28, 175, 188, 20);
+		contentPane.add(lblNmeroDeFaltas);
+		
+		textField = new JTextField();
+		textField.setBounds(231, 172, 146, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.setBounds(298, 199, 115, 29);
+		contentPane.add(btnOk);
+		
+		JButton btnAtrs = new JButton("Atr\u00E1s");
+		btnAtrs.setBounds(169, 199, 115, 29);
+		contentPane.add(btnAtrs);
+		
+		list = new JList();
+		list.setBounds(15, 44, 398, 115);
+		//contentPane.add(list); quitar esto para añadir el scroll
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportView(list);
+		scrollPane.setBounds(15, 44, 398, 115);
+		contentPane.add(scrollPane);
+		
+		cargarLista(diccionarioEstudiantes, profe);
+		
+		btnAtrs.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				atras.setVisible(true);
+				FaltaLeve.this.setVisible(false);
+			}
+		});
+		
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				String user = (String) list.getSelectedValue();
+
+				if (user != null) {
+					
+					String numero = textField.getText();
+					int numero1 = Integer.parseInt(numero);
+
+					Estudiante modificar = null;
+
+					for (Estudiante b : diccionarioEstudiantes) {
+						
+						if (b.getUser().equals(user)) {
+							modificar = b;
+							break;
+						}
+					}
+
+					modificar.setFaltaleve(numero1);
+					
+					try {
+			            SelectData.actualizarFaltaLeve(user, numero1);
+			            
+			        } catch (SQLException e1) {
+			            e1.printStackTrace();
+			        }
+					
+					JOptionPane.showMessageDialog(FaltaLeve.this, "Ya se ha realizado el cambio ");
+					
+				
+				}
+				FaltaLeve.this.setVisible(false);
+				atras.setVisible(true);
+				
+			}
+
+			
+
+			
+		});
+
+	}
+
+
+	private void cargarLista(ArrayList<Estudiante> diccionarioEstudiantes, Persona profe) {
+		// TODO Auto-generated method stub
+		
+		DefaultListModel lista = new DefaultListModel();
+
+		for (Estudiante a : diccionarioEstudiantes) {
+			
+			lista.addElement(a.getUser());
+		}
+		
+
+		list.setModel(lista);
+		
+	}
+}
