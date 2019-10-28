59d6ed03cd334d4ea6d3024c6dda2547359d0886
amaiazarranz <amaia.zarranz@opendeusto.es>
2019-10-28 16:55:40 +0100

    he conseguido enviar correos desde Java

---
+++ src/frontend/Enviar.java
@@ -0,0 +1,77 @@
+package frontend;
+
+import java.io.File;
+
+
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class Enviar 
+{
+	static boolean enviarConGmail(String user, String contrasena, String destinatario, String asunto, String cuerpo) {
+	    Properties emailProperties = new Properties();
+	    
+	    emailProperties.put("mail.transport.protocol", "smtp");
+	    emailProperties.put("mail.smtp.auth", "true");
+	    emailProperties.put("mail.smtp.port", "587");
+	    emailProperties.put("mail.smtp.socketFactory.port", "465");
+	    emailProperties.put("mail.smtp.host", "smtp.gmail.com");
+	    emailProperties.put("mail.smtp.ssl.enable", "true");
+	    emailProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+	    emailProperties.put("mail.debug", "true");
+	    emailProperties.put("mail.smtp.user", user);
+	    emailProperties.put("mail.smtp.password", contrasena); 
+	    emailProperties.put("mail.smtp.starttls.enable", "true");
+	    
+	 
+	 
+	    Session session = Session.getInstance(emailProperties, 
+	    		new javax.mail.Authenticator() {
+	    	protected PasswordAuthentication getPasswordAuthentication() {
+	    		return new PasswordAuthentication (user, contrasena);
+	    	}
+	    }
+	    		);
+	    session.setDebug(true);
+	    
+	    MimeMessage message = new MimeMessage(session);
+	    try {
+
+	    	BodyPart texto= new MimeBodyPart();
+	        texto.setText(cuerpo);
+
+	        MimeMultipart m=new MimeMultipart();
+	        m.addBodyPart(texto);
+
+	        message.setFrom(new InternetAddress(user));
+	        message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));   //Se podrían anadir varios de la misma manera
+	        message.setSubject(asunto);
+	        message.setContent(m);
+
+	        Transport transport = session.getTransport("smtp");
+	        transport.connect("smtp.gmail.com", user, contrasena);
+	        transport.sendMessage(message, message.getAllRecipients());
+
+	        transport.close();
+	        return true;
+    }
+    catch (MessagingException me) 
+	    {
+        return false;
+    }
+	}
+
+
+}
---
+++ src/frontend/EnviarCorreo.java
@@ -0,0 +1,122 @@
+package frontend;
+
+import java.awt.BorderLayout;
+
+import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import usuarios.Persona;
+
+public class EnviarCorreo extends JFrame {
+
+	private JPanel contentPane;
+	
+	CustomTextField destinatario;
+	CustomTextField asunto;
+	CustomTextField cuerpo;
+	JButton boton1;
+	JLabel imagen;
+	private File outputfile;
+
+	/**
+	 * Create the frame.
+	 */
+	
+	public EnviarCorreo(Persona u) {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		destinatario = new CustomTextField (0);
+		destinatario.setPlaceholder("DESTINATARIO");
+		destinatario.setBounds(58, 43, 300, 30);
+		contentPane.add(destinatario);
+		
+		asunto = new CustomTextField (0);
+		asunto.setPlaceholder("ASUNTO");
+		asunto.setBounds(58, 78, 300, 30);
+		contentPane.add(asunto);
+		
+		cuerpo = new CustomTextField(5);
+		cuerpo.setPlaceholder("CUERPO");
+		cuerpo.setBounds(58, 112, 300, 93);
+		contentPane.add(cuerpo);
+				
+		boton1 = new JButton("Enviar");
+		boton1.setBounds(123, 209, 300, 30);
+		contentPane.add(boton1);
+		
+	
+		EnviarCorreo.this.setVisible(true);
+		
+		boton1.addActionListener (new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+			
+				
+				if(destinatario.getText().toUpperCase().equals("destinatario") || cuerpo.getText().toUpperCase().equals("cuerpo"))
+				{
+					JOptionPane.showMessageDialog(null, "Uno de los campos está vació, rellénalo");
+				}
+				else
+				{
+					
+					String d = destinatario.getText();
+					String a = asunto.getText();
+					String c = cuerpo.getText();
+					
+					String user = null;
+					user = u.getEmail();
+					System.out.println(user);
+					
+					String contra = null;
+					contra = u.getPassword();
+					System.out.println(contra);
+					
+					if (Enviar.enviarConGmail(user, contra, d, a, c) == true)
+					{
+						JOptionPane.showMessageDialog(null, "Enviado");
+						int opcion = JOptionPane.showConfirmDialog(contentPane, "¿Quieres volver al principio?");
+						if(opcion == 0)
+						{
+							Principio f = new Principio();
+							EnviarCorreo.this.dispose();
+						}
+						else
+						{
+							System.exit(0);
+						}
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null, "No se ha podido enviar");
+					
+					}
+					
+					
+					
+					
+				}
+			}	
+		});	
+	}
+
+}
