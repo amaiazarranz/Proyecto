954e0f4df37521c80d02233b97fe18b4b9d06053
olatzgonzalez <olatz.gonzalez1@opendeusto.es>
2020-01-14 14:57:41 +0100

    Cambiar el código de la prueba unitaria del QuickSort

--- src/tests/QuickSortTest.java
+++ src/tests/QuickSortTest.java
@@ -1,13 +1,6 @@
 package tests;
 
 import static org.junit.Assert.*;
-
-/**
- * En esta clase se hacen las pruebas unitarias del MergeSort
- * @author Alumno Amaia y Olatz
- *
- */
-
 import java.util.ArrayList;
 
 import org.junit.After;
@@ -15,6 +8,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import utilidades.QuickSort;
+
+/**
+ * En esta clase se hacen las pruebas unitarias del MergeSort
+ * @author Alumno Amaia y Olatz
+ *
+ */
 
 public class QuickSortTest {
 	
@@ -51,22 +50,15 @@ public class QuickSortTest {
 	
 	@Test
 	public void testOrdenar() {
+		
 		QuickSort.quicksort(arr, 0, arr.length);
 		
-		int menor1= arr[0];
-		int menor2= arr[1];
-		int menor3= arr[2];
-		int menor4= arr[3];
-		int menor5= arr[4];
-		
-		assertEquals(menor1, 1000);
-		assertEquals(menor2, 1500);
-		assertEquals(menor3, 2000);
-		assertEquals(menor4, 2500);
-		assertEquals(menor5, 3000);
-		assertNotSame(menor1, 3000);
-		
-		
+		assertEquals(arr[0], 1000);
+		assertEquals(arr[1], 1500);
+		assertEquals(arr[2], 2000);
+		assertEquals(arr[3], 2500);
+		assertEquals(arr[4], 3000);
+		assertNotSame(arr[0], 3000); 
 	}
 
 }
